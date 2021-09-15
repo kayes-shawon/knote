2be02460e0f254c06607035507ac0a8773f9ad8a
kayes-shawon <shawon.kayes@gmail.com>
2021-09-16 01:37:09 +0600

    in progress api building

--- pkg/cmd/run.go
+++ pkg/cmd/run.go
@@ -1,13 +1,23 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/kayes-shawon/knote/pkg/models"
 	"log"
 	"net/http"
 )
 
 func createHandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	note := new(models.Note)
+
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "json: %+v", note)
 }
 
 func RunServer() {
--- pkg/models/note.go
+++ pkg/models/note.go
@@ -3,10 +3,10 @@ package models
 import "fmt"
 
 type Note struct {
-	Id int64
-	Title string
-	Content string
-	Tag string
+	Id int64 `json:"id"`
+	Title string `json:"title"`
+	Content string `json:"content"`
+	Tag string `json:"tag"`
 }
 
 func (n *Note) String() string  {
